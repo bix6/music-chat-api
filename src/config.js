9d6b9f10d113aef61a11919e0624937fb014772e
bix <840573+bix6@users.noreply.github.com>
2020-07-09 16:50:46 -0700

    prep deploy

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "config-no-node-env",
   API_TOKEN: process.env.API_TOKEN || "config-no-api-token",
-  DB_URL: process.env.DB_URL || "config-no-db-url",
+  DATABASE_URL: process.env.DATABASE_URL || "config-no-db-url",
 };

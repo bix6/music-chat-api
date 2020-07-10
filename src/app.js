4c28aeba4bc40e33feadcd4ba9ce1b834a0c55a1
bix <840573+bix6@users.noreply.github.com>
2020-07-10 08:54:24 -0700

    config CLIENT_ORIGIN

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
 const cookieParser = require("cookie-parser");
-const { NODE_ENV } = require("./config");
+const { NODE_ENV, CLIENT_ORIGIN } = require("./config");
 const validateBearerToken = require("./validate-bearer-token");
 const errorHandler = require("./error-handler");
 const chatroomsRouter = require("../src/chatrooms/chatrooms-router");
@@ -18,7 +18,11 @@ const morganOption = NODE_ENV === "production" ? "tiny" : "dev";
 
 app.use(morgan(morganOption));
 app.use(helmet());
-app.use(cors());
+app.use(
+  cors({
+    origin: CLIENT_ORIGIN,
+  })
+);
 app.use(validateBearerToken);
 app.use(cookieParser());
 
--- src/config.js
+++ src/config.js
@@ -3,4 +3,5 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || "config-no-node-env",
   API_TOKEN: process.env.API_TOKEN || "config-no-api-token",
   DATABASE_URL: process.env.DATABASE_URL || "config-no-db-url",
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || "config-no-client-origin",
 };

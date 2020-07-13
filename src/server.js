16c7ef7566638f75dc0aac145fc47bdaf1569f8d
bix <840573+bix6@users.noreply.github.com>
2020-07-13 11:46:04 -0700

    start WebSockets

--- src/server.js
+++ src/server.js
@@ -2,6 +2,11 @@ const knex = require("knex");
 const app = require("./app");
 const { PORT, DATABASE_URL } = require("./config");
 
+// TODO WebSocket
+// Setup for WebSocket
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
 const db = knex({
   client: "pg",
   connection: DATABASE_URL,
@@ -9,6 +14,15 @@ const db = knex({
 
 app.set("db", db);
 
-app.listen(PORT, () => {
+// TODO WebSocket
+// Test WebSocket connection
+// This is not logging currently
+io.on("connection", (socket) => {
+  console.log("user connected");
+});
+
+// TODO WebSocket
+// I switched this to http instead of app based on the example
+http.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
 });

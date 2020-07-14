9fa32ebc75cce03665a10fdc66a08b16f3d6eff9
bix <840573+bix6@users.noreply.github.com>
2020-07-14 08:11:57 -0700

    push for nick

--- src/server.js
+++ src/server.js
@@ -3,8 +3,11 @@ const app = require("./app");
 const { PORT, DATABASE_URL } = require("./config");
 
 // WebSocket Setup
+const server = require("http").Server(app);
+/*
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
+*/
 
 const db = knex({
   client: "pg",
@@ -14,14 +17,19 @@ const db = knex({
 app.set("db", db);
 
 // TODO WebSocket
+/*
 io.on("connection", (socket) => {
   console.log(socket.client.id + " connected");
   socket.on("chat message", (msg) => {
     console.log("message: " + msg);
     io.emit("chat message", msg);
   });
+  socket.on("connect message", (msg) => {
+    console.log("connect message: " + msg);
+  });
 });
+*/
 
-http.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
 });

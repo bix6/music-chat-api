2b90a19f3877986e02bb797b56ea409003d62ca3
bix <840573+bix6@users.noreply.github.com>
2020-07-16 16:53:19 -0700

    socket in progress

--- src/messages/messages-router.js
+++ src/messages/messages-router.js
@@ -3,85 +3,101 @@ const xss = require("xss");
 const path = require("path");
 const MessagesService = require("./messages-service");
 
-// module.exports = function (io) {
-const messagesRouter = express.Router();
-const jsonParser = express.json();
+module.exports = function (io) {
+  const messagesRouter = express.Router();
+  const jsonParser = express.json();
 
-// TODO socket
-// const io = require("socket.io");
-//   io.sockets.on('connection', function(socket) {
-//       console.log('Server-Client Connected!')
-//   })
+  // TODO socket
+  var sendResponse = () => {};
 
-//   socket.on("chat message", (msg) => {
-//     console.log("message: " + msg);
-//     io.emit("chat message", msg);
-//   });
+  io.on("connection", (socket) => {
+    console.log(socket.client.id + " connected");
 
-const sanitizeMessage = (message) => ({
-  id: message.id,
-  content_type: xss(message.content_type),
-  message: xss(message.message),
-  content_id: xss(message.content_id),
-  chatroom_id: xss(message.chatroom_id),
-  person_id: xss(message.person_id),
-  username: xss(message.name),
-});
+    socket.on("connected", (data) => {
+      // TODO
+      // listen to event at any time (not just when endpoint is called)
+      // execute some code
+    });
 
-messagesRouter.route("/:message_id").get((req, res, next) => {
-  MessagesService.getByMessageId(req.app.get("db"), req.params.message_id)
-    .then((messages) => {
-      res.json(messages.map(sanitizeMessage));
-    })
-    .catch(next);
-});
+    socket.on("chat message", (data) => {
+      console.log("server on chat message");
+      sendResponse(data);
+    });
+  });
 
-messagesRouter.route("/chatroom/:chatroom_id").get((req, res, next) => {
-  MessagesService.getByChatroomId(req.app.get("db"), req.params.chatroom_id)
-    .then((message) => {
-      res.json(message.map(sanitizeMessage));
-    })
-    .catch(next);
-});
+  const sanitizeMessage = (message) => ({
+    id: message.id,
+    content_type: xss(message.content_type),
+    message: xss(message.message),
+    content_id: xss(message.content_id),
+    chatroom_id: xss(message.chatroom_id),
+    person_id: xss(message.person_id),
+    username: xss(message.name),
+  });
 
-messagesRouter.route("/").post(jsonParser, (req, res, next) => {
-  const {
-    content_type,
-    message,
-    content_id,
-    chatroom_id,
-    person_id,
-  } = req.body;
-  const newMessage = {
-    content_type,
-    message,
-    content_id,
-    chatroom_id,
-    person_id,
-  };
+  messagesRouter.route("/:message_id").get((req, res, next) => {
+    MessagesService.getByMessageId(req.app.get("db"), req.params.message_id)
+      .then((messages) => {
+        res.json(messages.map(sanitizeMessage));
+      })
+      .catch(next);
+  });
 
-  for (const [key, value] of Object.entries(newMessage)) {
-    if (value == null) {
-      return res.status(400).json({
-        error: { message: `Missing ${key} in request body` },
-      });
+  messagesRouter.route("/chatroom/:chatroom_id").get((req, res, next) => {
+    MessagesService.getByChatroomId(req.app.get("db"), req.params.chatroom_id)
+      .then((message) => {
+        res.json(message.map(sanitizeMessage));
+      })
+      .catch(next);
+  });
+
+  messagesRouter.route("/").post(jsonParser, (req, res, next) => {
+    const {
+      content_type,
+      message,
+      content_id,
+      chatroom_id,
+      person_id,
+    } = req.body;
+    const newMessage = {
+      content_type,
+      message,
+      content_id,
+      chatroom_id,
+      person_id,
+    };
+
+    for (const [key, value] of Object.entries(newMessage)) {
+      if (value == null) {
+        return res.status(400).json({
+          error: { message: `Missing ${key} in request body` },
+        });
+      }
     }
-  }
-  // This is needed to remove the username from the payload
-  // so the insert doesn't throw an error.
-  // The username is only needed for the GET
-  const noUsernameMessage = sanitizeMessage(newMessage);
-  delete noUsernameMessage["username"];
+    // This is needed to remove the username from the payload
+    // so the insert doesn't throw an error.
+    // The username is only needed for the GET
+    const noUsernameMessage = sanitizeMessage(newMessage);
+    delete noUsernameMessage["username"];
 
-  MessagesService.insertMessage(req.app.get("db"), noUsernameMessage)
-    .then((message) => {
-      res
-        .status(201)
-        .location(path.posix.join(req.originalUrl, `/${message.id}`))
-        .json(sanitizeMessage(message));
-    })
-    .catch(next);
-});
-// };
+    MessagesService.insertMessage(req.app.get("db"), noUsernameMessage)
+      .then((message) => {
+        // https://stackoverflow.com/questions/56219098/make-socket-io-work-within-an-api-endpoint-in-node-js
+        // The send response and res logic is going to need some work
+        // TODO
+        sendResponse = (data) => {
+          io.emit("chat message", data);
+        };
+        res
+          .status(201)
+          .location(path.posix.join(req.originalUrl, `/${message.id}`))
+          .json(sanitizeMessage(message));
+      })
+      .catch(next);
+  });
 
-module.exports = messagesRouter;
+  return messagesRouter;
+};
+
+// TODO
+// module.exports = messagesRouter;
--- src/server.js
+++ src/server.js
@@ -1,11 +1,27 @@
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+const cookieParser = require("cookie-parser");
+const validateBearerToken = require("./validate-bearer-token");
+const errorHandler = require("./error-handler");
+const chatroomsRouter = require("../src/chatrooms/chatrooms-router");
+const personsRouter = require("../src/persons/persons-router");
+// const logger = require('./logger');
 const knex = require("knex");
-const app = require("./app");
-const { PORT, DATABASE_URL } = require("./config");
+const { PORT, DATABASE_URL, NODE_ENV, CLIENT_ORIGIN } = require("./config");
 
-// WebSocket Setup
+// TODO socket
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const messagesRouter = require("../src/messages/messages-router")(io);
+
+// WebSocket setup
 // const server = require("http").Server(app);
-// const http = require("http").createServer(app);
-// const io = require("socket.io")(http);
+// const io = require("socket.io")(server);
+// const messagesRouter = require("../src/messages/messages-router")(io);
 
 const db = knex({
   client: "pg",
@@ -14,20 +30,26 @@ const db = knex({
 
 app.set("db", db);
 
-// TODO WebSocket
-/*
-io.on("connection", (socket) => {
-  console.log(socket.client.id + " connected");
-  socket.on("chat message", (msg) => {
-    console.log("message: " + msg);
-    io.emit("chat message", msg);
-  });
-  socket.on("connect message", (msg) => {
-    console.log("connect message: " + msg);
-  });
-});
-*/
+const morganOption = NODE_ENV === "production" ? "tiny" : "dev";
 
-app.listen(PORT, () => {
+app.use(morgan(morganOption));
+app.use(helmet());
+app.use(
+  cors({
+    origin: CLIENT_ORIGIN,
+  })
+);
+app.use(validateBearerToken);
+app.use(cookieParser());
+
+app.use("/api/chatrooms", chatroomsRouter);
+app.use("/api/messages", messagesRouter);
+app.use("/api/persons", personsRouter);
+
+app.use(errorHandler);
+
+module.exports = app;
+
+server.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
 });

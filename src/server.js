c24523371ec4da90e1adccbf010f54fb45c5b8ae
bix <840573+bix6@users.noreply.github.com>
2020-07-23 16:08:10 -0700

    adjust cors to allow socket

--- src/server.js
+++ src/server.js
@@ -28,11 +28,7 @@ const morganOption = NODE_ENV === "production" ? "tiny" : "dev";
 
 app.use(morgan(morganOption));
 app.use(helmet());
-app.use(
-  cors({
-    origin: CLIENT_ORIGIN,
-  })
-);
+app.use(cors());
 app.use(validateBearerToken);
 
 app.use("/api/chatrooms", chatroomsRouter);
